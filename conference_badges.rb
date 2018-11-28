affb5922d25f23c544e8726e1e66416633837707
Benjamin M Lancaster <bmlancaster07@gmail.com>
2018-11-28 20:33:31 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,13 @@
 # Write your code here.
+people = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+def badge_maker(name)
+  return "Hello my name is #{name}"
+  
+def assign_rooms(array)
+  assignment = []
+  array.each do |name|
+    assignment.push("Hello my name is #{name}".)
+  end
+    return assignment
+end
+
